f50aaed2843b40492d8b30775076c74460a26f91
roman <romanze1706@gmail.com>
2023-09-16 09:45:17 +0300

    Создан проект

---
+++ src/main/java/ru/service/ticketsales/TicketSalesApplication.java
@@ -0,0 +1,13 @@
+package ru.service.ticketsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketSalesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketSalesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/service/ticketsales/TicketSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.service.ticketsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketSalesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
